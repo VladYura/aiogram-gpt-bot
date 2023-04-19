a5876be4aaac85f099470e2d0fa34cb561064a52
vlad_linux <mail@mail.ru>
2023-04-19 22:08:42 +0300

    First commit

---
+++ bot.py
@@ -0,0 +1,42 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import openai
+from config import TOKEN, GPT_TOKEN
+import logging
+
+openai.api_key = GPT_TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+model_engine = "text-davinci-003"
+logging.basicConfig(level=logging.INFO)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Привет!\nНапиши мне что-нибудь')
+
+
+@dp.message_handler()
+async def process_help_command(message: types.Message):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": message.text}
+        ]
+    )
+    text = completion.choices[0].message.content
+    output = "\n".join([text[i:i+55] for i in range(0, len(text), 55)])
+    print('-------------------------------------------------------')
+    print(f'User - {message.from_user.first_name} {message.from_user.last_name}')
+    print(f'\nMessage - {message.text}')
+    print(f'\nAnswer:')
+    print(output)
+    print('-------------------------------------------------------\n')
+    await bot.send_message(message.from_user.id, completion.choices[0].message.content)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
